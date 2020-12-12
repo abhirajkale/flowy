c165f5819899781471b15103c63b673fc187106b
abhirajkale <abhirajpkale@gmail.com>
2020-12-12 12:43:32 -0500

    replaced static html with reusable react components

--- demo/main.js
+++ demo/main.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function(){
     var rightcard = false;
     var tempblock;
     var tempblock2;
-    document.getElementById("blocklist").innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>';
     flowy(document.getElementById("canvas"), drag, release, snapping);
     function addEventListenerMulti(type, listener, capture, selector) {
         var nodes = document.querySelectorAll(selector);
@@ -15,10 +14,19 @@ document.addEventListener("DOMContentLoaded", function(){
         grab.parentNode.removeChild(grab);
         var blockin = drag.querySelector(".blockin");
         blockin.parentNode.removeChild(blockin);
+        var existingHtml = drag.innerHTML;
         if (drag.querySelector(".blockelemtype").value == "1") {
-            drag.innerHTML += "<div class='blockyleft'><img src='assets/eyeblue.svg'><p class='blockyname'>New visitor</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When a <span>new visitor</span> goes to <span>Site 1</span></div>";
+            ReactDOM.render(React.createElement(FlowCard, {
+              title: "action is performed",
+              subtitle: "Triggers when somebody performs a specified action",
+              src: "assets/action.svg"
+            }), drag);
         } else if (drag.querySelector(".blockelemtype").value == "2") {
-            drag.innerHTML += "<div class='blockyleft'><img src='assets/actionblue.svg'><p class='blockyname'>Action is performed</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>Action 1</span> is performed</div>";
+            ReactDOM.render(React.createElement(FlowCard, {
+              title: "action is performed",
+              subtitle: "Triggers when somebody performs a specified action",
+              src: "assets/action.svg"
+            }), drag);
         } else if (drag.querySelector(".blockelemtype").value == "3") {
             drag.innerHTML += "<div class='blockyleft'><img src='assets/timeblue.svg'><p class='blockyname'>Time has passed</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>10 seconds</span> have passed</div>";
         } else if (drag.querySelector(".blockelemtype").value == "4") {
@@ -38,6 +46,7 @@ document.addEventListener("DOMContentLoaded", function(){
         } else if (drag.querySelector(".blockelemtype").value == "11") {
             drag.innerHTML += "<div class='blockyleft'><img src='assets/errorred.svg'><p class='blockyname'>Prompt an error</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Trigger <span>Error 1</span></div>";
         }
+        drag.innerHTML += existingHtml
         return true;
     }
     function drag(block) {
